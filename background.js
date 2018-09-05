bbff6c5f4e9052765e13d0c1901403946db907eb
Jordan Majd <jordan@madtitanstudios.com>
2018-09-05 10:48:18 -0600

    Toolbar button toggles JS for current tab and all future tabs.

--- background.js
+++ background.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+var enabled = true;
+
 var pattern = '<all_urls>';
 
 var cspHeader = {
@@ -9,17 +11,66 @@ var cspHeader = {
 	value: "script-src 'none';"
 };
 
-browser.webRequest.onHeadersReceived.addListener(
-	setCSPHeader,
-	{ urls: [pattern] },
-	['blocking', 'responseHeaders']
-);
+var onIcon = {
+	48: "icons/on.svg",
+	96: "icons/on.svg"
+};
+
+var offIcon = {
+	48: "icons/off.svg",
+	96: "icons/off.svg"
+};
+
+browser.browserAction.onClicked.addListener(toggleScript);
+
+
+function enableJS(){
+
+	console.log("Enabling javascript");
+
+	browser.browserAction.setIcon({path:onIcon});
+	browser.browserAction.setTitle({title: "Script Switch: JS enabled"});
+
+	browser.webRequest.onHeadersReceived.removeListener(setCSPHeader);
+}
+
+
+function disableJS(){
+
+	console.log("Disabling javascript");
+
+	browser.browserAction.setIcon({path:offIcon});
+	browser.browserAction.setTitle({title: "Script Switch: JS disabled"});
+
+	browser.webRequest.onHeadersReceived.addListener(
+		setCSPHeader,
+		{ urls: [pattern] },
+		['blocking', 'responseHeaders']
+	);
+}
 
 
 function setCSPHeader(res){
-	console.log("Setting headers...");
+
+	console.log("Overriding Content Security Policy...");
+
 	res.responseHeaders.push(cspHeader);
+
 	return { responseHeaders: res.responseHeaders };
+}
+
+
+function toggleScript(){
+
+	enabled = !enabled;
+
+	if(enabled){
+		enableJS();
+	} else {
+		disableJS();
+	}
+
+	browser.tabs.reload({bypassCache:true});
 }
 
 })();
